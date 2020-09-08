6c4b5d63c061d2561233d0e9d1012304f247ead8
AKP-13 <alexpeirson@hotmail.com>
2020-09-08 23:42:29 +0100

    Final commit Tuesday eve

--- client/src/Containers/Dashboard.js
+++ client/src/Containers/Dashboard.js
@@ -9,67 +9,81 @@ const Dashboard = ({setAuth}) => {
     // let frequency;
     // let habit;
 
-  const [name, setName] = useState("");
-  const [freq, setFreq] = useState("");
-  const [habit, setHabit] = useState("");
-  const habits = []
-  const freqs = []
+    const [name, setName] = useState("");
+    const [freq, setFreq] = useState("");
+    const [habit, setHabit] = useState("");
+
+    // const freqs = []
 
 
-  const getName = async () => {
-    try {
-      const res = await fetch("http://localhost:5000/dashboard/", {
-        method: "GET",
-        headers: { token: localStorage.token }
-      });
+    const getName = async () => {
+        try {
+            const res = await fetch("http://localhost:5000/dashboard/", {
+                method: "GET",
+                headers: { token: localStorage.token }
+            });
 
-      const parseRes = await res.json();
-      console.log(parseRes);
-      
-    //   name = parseRes.user_name
-    //   frequency = parseRes.frequency
-    //   habit = parseRes.habit
-      // for (let i = 0; i < parseRes.length; i++) {
-      //   habits.push(parseRes[i].habit)
-      //   freqs.push(parseRes[i].frequency)
-      // }
-    //   console.log(habits)
-    //   console.log(freqs)
-      setName(parseRes.user_name);
-      setFreq(parseRes.frequency);
-      setHabit(parseRes.habit);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
+            const parseRes = await res.json();
+            console.log(parseRes);
+            
+            //   name = parseRes.user_name
+            //   frequency = parseRes.frequency
+            //   habit = parseRes.habit
+                // for (let i = 0; i < parseRes.length; i++) {
+                //   habits.push(parseRes[i].habit)
+                //   freqs.push(parseRes[i].frequency)
+                // }
+            //   console.log(habits)
+            //   console.log(freqs)
 
-  const logout = async e => {
-    e.preventDefault();
-    try {
-      localStorage.removeItem("token");
-      setAuth(false);
-      toast.success("Logout successfully");
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
+            setName(parseRes[0].user_name);
+            // setFreq(parseRes.frequency);
+            // setHabit(parseRes.habit);
+            let randomString = "hello there!"
 
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
 
-  useEffect(() => {
-   getName();
- }, []);
+    const logout = async e => {
+        e.preventDefault();
+        try {
+            localStorage.removeItem("token");
+            setAuth(false);
+            toast.success("Logout successfully");
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
 
 
-  return (
-    <Fragment>
-        <h1> Hello {name} </h1>
+    useEffect(() => {
+        getName();
+    }, []);
 
-        <button onClick={e => logout(e)} className="btn btn-primary">
-            Logout
-        </button>
-        <HabitList name={name} frequency={freq} habit={habit}/>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <h1> Hello {name} </h1>
+
+            <button onClick={e => logout(e)} className="btn btn-primary">
+                Logout
+            </button>
+            {/* <div>
+                <HabitList name={name} frequency={freq} habit={habit}/>
+            </div>
+
+            <HabitList name={name} frequency={freq} habit={habit}/> */}
+
+
+            {/* <div>
+                {parseRes.map((item, idx) => {
+                    return <li key={idx}>{item}<HabitList /></li>;
+                })}
+            </div> */}
+            
+        </Fragment>
+    );
 };
 
 export default Dashboard;
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -5,13 +5,13 @@ const pool = require("../db/db");
 router.get("/", authorize, async (req, res) => {
   try {
     const user = await pool.query(
-      // "SELECT user_name FROM users WHERE user_id = $1",
-    //   'SELECT * FROM habits WHERE user_id = $1',
-    'SELECT users.user_name, habits.habit, habits.frequency FROM habits LEFT JOIN users ON habits.user_id = users.user_id WHERE habits.user_id = $1',
-      [req.user]
+        // "SELECT user_name FROM users WHERE user_id = $1",
+        // 'SELECT * FROM habits WHERE user_id = $1',
+        'SELECT users.user_name, habits.habit, habits.frequency FROM habits LEFT JOIN users ON habits.user_id = users.user_id WHERE habits.user_id = $1',
+        [req.user]
     );
-    // console.log(req.user)
-    res.json(user.rows[0]);
+    // console.log(user.rows);
+    res.json(user.rows);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
