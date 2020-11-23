de381179a0c9d20ae289322bd9859a602514f051
Angus Woodman <>
2020-11-23 13:35:03 +0000

    change welcome page to correct styling

--- client/src/Containers/WelcomePage.js
+++ client/src/Containers/WelcomePage.js
@@ -5,12 +5,17 @@ import '../styles/WelcomePage.css';
 class WelcomePage extends React.Component {
     render() {
         return (
-            <>
-                <h1>welcome to our habitapp</h1>
-                <p>How to use our app</p>
-                <Link id='linkLogin' to="/login">Login</Link>
-                <Link id='linkRegister' to="/register">Register</Link>
-            </>
+            <section id='welcomePage'>
+                <h1 id='homepageLogo'>Habitapp</h1>
+                <div>
+                    <Link to="/login">
+                        <button className="homepageButtons">Login</button>
+                    </Link>
+                    <Link to="/register">
+                        <button className="homepageButtons">Register</button>
+                    </Link>
+                </div>
+            </section>
         )
     }
 }
